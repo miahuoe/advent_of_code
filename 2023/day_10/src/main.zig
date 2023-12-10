6a68a49c1f00ac8dcc8d03ab518db815c1d76259
Michał Czarnecki <czarnecky@va.pl>
2023-12-10 21:11:27 +0100

    Add Day 10 part 2, refactor

--- 2023/day_10/src/main.zig
+++ 2023/day_10/src/main.zig
@@ -7,9 +7,14 @@ const Node = struct {
 	dist: usize
 };
 
-fn cmp(_: void, a: Node, b: Node) std.math.Order {
-	return std.math.order(a.dist, b.dist);
-}
+const PipeScanState = enum {
+	Outside,
+	Inside,
+	FromInsideAtPipeFromUp,
+	FromInsideAtPipeFromDown,
+	FromOutsideAtPipeFromUp,
+	FromOutsideAtPipeFromDown,
+};
 
 const Map = struct {
 	w: i64,
@@ -19,6 +24,7 @@ const Map = struct {
 	cells_len: usize,
 	cells: []u8,
 	loop: []bool,
+	cat: []u8,
 
 	pub fn idx_of(self: *Map, x: i64, y: i64) ?usize {
 		if (x < 0 or self.w <= x or y < 0 or self.h <= y) {
@@ -38,62 +44,15 @@ const Map = struct {
 		return pos;
 	}
 
-	pub fn neighbours_of(self: *Map, id: usize) [2]?usize {
+	pub fn neighbours_of(self: *Map, id: usize) [2]usize {
 		var nc: usize = 0;
-		var ns = [2]?usize{null, null};
+		var ns = [2]usize{0, 0};
 		const pos = self.pos_of(id) orelse return ns;
 		const c = self.cells[id];
-		var dsc: usize = 0;
 		var dxs: [2]i64 = undefined;
 		var dys: [2]i64 = undefined;
 		switch (c) {
-		else => return ns,
-		'S' => {
-			const l_id = self.idx_of(pos[0]-1, pos[1]+0);
-			if (l_id) |l| {
-				switch (self.cells[l]) {
-				else => {},
-				'-','L','F' => {
-					dxs[dsc] = -1;
-					dys[dsc] = 0;
-					dsc += 1;
-				},
-				}
-			}
-			const r_id = self.idx_of(pos[0]+1, pos[1]+0);
-			if (r_id) |r| {
-				switch (self.cells[r]) {
-				else => {},
-				'-','J','7' => {
-					dxs[dsc] = 1;
-					dys[dsc] = 0;
-					dsc += 1;
-				},
-				}
-			}
-			const u_id = self.idx_of(pos[0]+0, pos[1]+1);
-			if (u_id) |u| {
-				switch (self.cells[u]) {
-				else => {},
-				'|','7','F' => {
-					dxs[dsc] = 0;
-					dys[dsc] = 1;
-					dsc += 1;
-				},
-				}
-			}
-			const d_id = self.idx_of(pos[0]+0, pos[1]-1);
-			if (d_id) |d| {
-				switch (self.cells[d]) {
-				else => {},
-				'|','L','J' => {
-					dxs[dsc] = 0;
-					dys[dsc] = -1;
-					dsc += 1;
-				},
-				}
-			}
-		},
+		else => unreachable,
 		'F' => {
 			dxs[0] = 0;
 			dys[0] = 1;
@@ -144,13 +103,16 @@ const Map = struct {
 				nc += 1;
 			}
 		}
+		std.debug.assert(nc == 2);
 		return ns;
 	}
 
-	pub fn analyze(self: *Map) void {
+	pub fn prepare(self: *Map) void {
 		// calculate width, height
 		// find starting position
 		// compress by removing \n
+		// replace S with pipe
+
 		self.w = 0;
 		self.h = 0;
 		var x: i64 = 0;
@@ -177,34 +139,233 @@ const Map = struct {
 		if (x > 0) {
 			self.h += 1;
 		}
+
+		const id = self.idx_of(self.sx, self.sy) orelse unreachable;
+		var L = false;
+		var R = false;
+		var U = false;
+		var D = false;
+		const l_id = self.idx_of(self.sx-1, self.sy+0);
+		if (l_id) |l| {
+			switch (self.cells[l]) {
+			else => {},
+			'-','L','F' => {
+				L = true;
+			},
+			}
+		}
+		const r_id = self.idx_of(self.sx+1, self.sy+0);
+		if (r_id) |r| {
+			switch (self.cells[r]) {
+			else => {},
+			'-','J','7' => {
+				R = true;
+			},
+			}
+		}
+		const d_id = self.idx_of(self.sx+0, self.sy+1);
+		if (d_id) |d| {
+			switch (self.cells[d]) {
+			else => {},
+			'|','L','J' => {
+				D = true;
+			},
+			}
+		}
+		const u_id = self.idx_of(self.sx+0, self.sy-1);
+		if (u_id) |u| {
+			switch (self.cells[u]) {
+			else => {},
+			'|','F','7' => {
+				U = true;
+			},
+			}
+		}
+
+		if (L and U) {
+			self.cells[id] = 'J';
+		} else if (L and R) {
+			self.cells[id] = '-';
+		} else if (L and D) {
+			self.cells[id] = '7';
+		} else if (R and U) {
+			self.cells[id] = 'L';
+		} else if (R and D) {
+			self.cells[id] = 'F';
+		} else if (U and D) {
+			self.cells[id] = '|';
+		} else {
+			unreachable;
+		}
 	}
 
-	pub fn walk_on_loop(self: *Map, allocator: std.mem.Allocator) !usize {
-		_ = allocator;
-		var start = self.idx_of(self.sx, self.sy) orelse 0; // TODO
+	pub fn count_inside(self: *Map) usize {
+		@memset(self.cat, '?');
+		for (0..@intCast(self.h)) |y| {
+			var st: PipeScanState = .Outside;
+			for (0..@intCast(self.w)) |x| {
+				const I = self.idx_of(@intCast(x), @intCast(y)) orelse continue;
+				const C = self.cells[I];
+				const L = self.loop[I];
+				var in: bool = false;
+				switch (st) {
+					.Outside => {
+						if (L) {
+							switch (C) {
+								else => unreachable,
+								'|' => {
+									st = .Inside;
+								},
+								'F' => {
+									st = .FromOutsideAtPipeFromDown;
+								},
+								'L' => {
+									st = .FromOutsideAtPipeFromUp;
+								},
+							}
+						}
+					},
+					.Inside => {
+						if (L) {
+							switch (C) {
+								else => unreachable,
+								'|' => {
+									st = .Outside;
+								},
+								'F' => {
+									st = .FromInsideAtPipeFromDown;
+								},
+								'L' => {
+									st = .FromInsideAtPipeFromUp;
+								},
+							}
+						} else {
+							in = true;
+						}
+					},
+					.FromInsideAtPipeFromUp => {
+						switch (C) {
+							else => unreachable,
+							'-' => {},
+							'J' => {
+								if (L) {
+									st = .Inside;
+								} else {
+									unreachable;
+								}
+							},
+							'7' => {
+								if (L) {
+									st = .Outside;
+								} else {
+									unreachable;
+								}
+							},
+						}
+					},
+					.FromInsideAtPipeFromDown => {
+						switch (C) {
+							else => unreachable,
+							'-' => {},
+							'J' => {
+								if (L) {
+									st = .Outside;
+								} else {
+									unreachable;
+								}
+							},
+							'7' => {
+								if (L) {
+									st = .Inside;
+								} else {
+									unreachable;
+								}
+							},
+						}
+					},
+					.FromOutsideAtPipeFromUp => {
+						switch (C) {
+							else => unreachable,
+							'-' => {},
+							'J' => {
+								if (L) {
+									st = .Outside;
+								} else {
+									unreachable;
+								}
+							},
+							'7' => {
+								if (L) {
+									st = .Inside;
+								} else {
+									unreachable;
+								}
+							},
+						}
+					},
+					.FromOutsideAtPipeFromDown => {
+						switch (C) {
+							else => unreachable,
+							'-' => {},
+							'J' => {
+								if (L) {
+									st = .Inside;
+								} else {
+									unreachable;
+								}
+							},
+							'7' => {
+								if (L) {
+									st = .Outside;
+								} else {
+									unreachable;
+								}
+							},
+						}
+					},
+				}
+				if (in) {
+					self.cat[I] = 'I';
+				} else if (L) {
+					self.cat[I] = C;
+				} else {
+					self.cat[I] = 'O';
+				}
+			}
+		}
+		var inside: usize = 0;
+		for (0..@intCast(self.w*self.h)) |i| {
+			if (self.cat[i] == 'I') {
+				inside += 1;
+			}
+		}
+		return inside;
+	}
+
+	pub fn walk_on_loop(self: *Map) usize {
+		var start = self.idx_of(self.sx, self.sy) orelse unreachable;
 		const start_neighbours = self.neighbours_of(start);
 		var prev = start;
-		var current = start_neighbours[0] orelse start;
+		var current = start_neighbours[0];
+		@memset(self.loop, false);
 		self.loop[start] = true;
 		self.loop[current] = true;
 
 		var steps: usize = 1;
 		main: while (true) {
 			const neighbours = self.neighbours_of(current);
-			for (neighbours) |maybe_n| {
-				if (maybe_n) |n| {
-					if (n == prev) {
-						continue;
-					}
-					self.loop[n] = true;
-					if (n == start) {
-						break :main;
-					}
-					prev = current;
-					current = n;
-					steps += 1;
-					continue :main;
+			for (neighbours) |n| {
+				if (n == prev) {
+					continue;
 				}
+				self.loop[n] = true;
+				if (n == start) {
+					break :main;
+				}
+				prev = current;
+				current = n;
+				steps += 1;
+				continue :main;
 			}
 		}
 		return steps;
@@ -215,47 +376,26 @@ pub fn main() !void {
 	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 	const allocator = gpa.allocator();
 
-	var args = std.process.args();
-	_ = args.skip();
-
-	var iterations: usize = 1;
-	if (args.next()) |arg_iter| {
-		iterations = try std.fmt.parseInt(usize, arg_iter, 10);
-	}
-
 	const stdin = std.io.getStdIn().reader();
-
-	var stdout = std.io.getStdOut();
-	var bw = std.io.bufferedWriter(stdout.writer());
-	const w = bw.writer();
+	const stdout = std.io.getStdOut().writer();
 
 	var map: Map = undefined;
+
 	map.cells = try allocator.alloc(u8, 1<<20);
+	defer allocator.free(map.cells);
+
 	map.loop = try allocator.alloc(bool, 1<<20);
+	defer allocator.free(map.loop);
+
+	map.cat = try allocator.alloc(u8, 1<<20);
+	defer allocator.free(map.cat);
 
 	map.cells_len = try stdin.readAll(map.cells);
+	map.prepare();
 
-	map.analyze();
-
-	for (0..iterations) |_| {
-		const steps = try map.walk_on_loop(allocator);
-		if (false) {
-			var y: i64 = 0;
-			while (y < map.h) : (y += 1) {
-				var x: i64 = 0;
-				while (x < map.w) : (x += 1) {
-					const idx = map.idx_of(x, y);
-					if (idx) |i| {
-						const b: usize = if (map.loop[i]) 1 else 0;
-						try w.print("{d}", .{b});
-					}
-				}
-				try w.print("\n", .{});
-			}
-			try w.print("\n", .{});
-		}
-		try w.print("part 1: {d}\n", .{steps/2+1});
-		try bw.flush();
-	}
+	const steps = map.walk_on_loop();
+	const inside = map.count_inside();
+	try stdout.print("part 1: {d}\n", .{steps/2+1});
+	try stdout.print("part 2: {d}\n", .{inside});
 }
 
