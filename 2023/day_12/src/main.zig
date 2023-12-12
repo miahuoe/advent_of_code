76079e94b8fb3a3676b8b84882ad7c125b6b829c
Michał Czarnecki <czarnecky@va.pl>
2023-12-12 23:18:06 +0100

    Add Day 12 part 1

--- 2023/day_12/src/main.zig
+++ 2023/day_12/src/main.zig
@@ -6,8 +6,8 @@ const SpringArray = struct {
 	springs_count: usize,
 	springs: [64]u8,
 
-	arrangements: usize,
 	stack: [64]u8,
+	H: std.AutoHashMap(usize, usize),
 
 	pub fn from_line(line: []u8) SpringArray {
 		var sa: SpringArray = undefined;
@@ -34,7 +34,7 @@ const SpringArray = struct {
 			switch (c) {
 			'0'...'9' => {
 				if (num) |n| {
-					num = (n * 1) + (c - '0');
+					num = (n * 10) + (c - '0');
 				} else {
 					num = (c - '0');
 				}
@@ -56,74 +56,48 @@ const SpringArray = struct {
 		return sa;
 	}
 
-	pub fn analyze(self: *SpringArray) usize {
-		self.arrangements = 0;
-		self._analyze('.', 0, -1, 0);
-		return self.arrangements;
+	pub fn analyze(self: *SpringArray) !usize {
+		self.H.clearRetainingCapacity();
+		return try self._analyze('.', 0, 0, 0);
 	}
 
-	pub fn _analyze(self: *SpringArray, prev: u8, si: usize, bi: i64, b_rem: usize) void {
-		self.stack[si] = prev;
-		if (si == self.springs_count) {
-			if (b_rem == 0 and bi == self.blocks_count-1) {
-				std.log.debug("[{s}]", .{self.stack[1..1+si]});
-				self.arrangements += 1;
-			}
-			return;
+	pub fn _analyze(self: *SpringArray, prev: u8, i: usize, bi: usize, count: usize) !usize {
+		const key = count*self.blocks_count*self.springs_count + bi*self.springs_count + i;
+		var cached = self.H.get(key);
+		if (cached) |c| {
+			_ = c;
+			//return c;
 		}
-		const curr = self.springs[si];
-		switch (prev) {
-		else => {},
-		'.' => {
-			switch (curr) {
-			else => {},
-			'?' => {
-				if (bi+1 < self.blocks_count) {
-					self._analyze('#', si+1, bi+1, self.blocks[@intCast(bi+1)]-1);
-				}
-				if (b_rem == 0) {
-					self._analyze('.', si+1, bi, b_rem);
-				}
-			},
-			'.' => {
-				if (b_rem == 0) {
-					self._analyze('.', si+1, bi, b_rem);
-				}
-			},
-			'#' => {
-				if (bi+1 < self.blocks_count and b_rem == 0) {
-					self._analyze('#', si+1, bi+1, self.blocks[@intCast(bi+1)]-1);
-				}
-			},
+		self.stack[i] = prev;
+		if (i == self.springs_count) {
+			//std.log.debug("[{s}] ({d}/{d} {d}/{d})", .{self.stack[1..1+i], bi, self.blocks_count, count, self.blocks[bi]});
+			if ((bi == self.blocks_count and count == 0) or (bi == self.blocks_count-1 and count == self.blocks[bi])) {
+				//std.log.debug("[{s}] {any}", .{self.stack[1..1+i], self.blocks[0..self.blocks_count]});
+				return 1;
 			}
-		},
-		'#' => {
-			switch (curr) {
-			else => {},
-			'?' => {
-				if (b_rem == 0) {
-					self._analyze('.', si+1, bi, b_rem);
-				} else {
-					self._analyze('#', si+1, bi, b_rem-1);
-				}
-			},
-			'.' => {
-				if (b_rem == 0) {
-					self._analyze('.', si+1, bi, b_rem);
-				}
-			},
-			'#' => {
-				if (b_rem > 0) {
-					self._analyze('#', si+1, bi, b_rem-1);
-				}
-			},
-			}
-		},
+			return 0;
 		}
+		var c: usize = 0;
+		const curr = self.springs[i];
+		if (curr == '?' or curr == '.') {
+			if (count == 0) {
+				c += try self._analyze('.', i+1, bi, 0);
+			} else if (bi < self.blocks_count and count == self.blocks[bi]) {
+				c += try self._analyze('.', i+1, bi+1, 0);
+			}
+		}
+		if (curr == '?' or curr == '#') {
+			c += try self._analyze('#', i+1, bi, count+1);
+		}
+		try self.H.put(key, c);
+		return c;
 	}
 };
 
 pub fn main() !void {
+	var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+	const allocator = gpa.allocator();
+
 	const stdin = std.io.getStdIn().reader();
 	const stdout = std.io.getStdOut().writer();
 
@@ -134,18 +108,17 @@ pub fn main() !void {
 			continue;
 		}
 		var springs = SpringArray.from_line(line);
-		try stdout.print("       [", .{});
 		for (0..springs.springs_count) |s| {
 			try stdout.print("{c}", .{springs.springs[s]});
 		}
-		try stdout.print("] ", .{});
 		for (0..springs.blocks_count) |b| {
 			try stdout.print(" {d}", .{springs.blocks[b]});
 		}
-		try stdout.print("\n", .{});
-		const ar = springs.analyze();
+		springs.H = std.AutoHashMap(usize, usize).init(allocator);
+		defer springs.H.deinit();
+		const ar = try springs.analyze();
 		sum += ar;
-		try stdout.print("{d}\n\n", .{ar});
+		try stdout.print(" -> {d}\n", .{ar});
 	}
 	try stdout.print("{d}\n", .{sum});
 }
